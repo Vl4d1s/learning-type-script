76116655c1af28ec02149a4669d877fc52883d6c
Vladis <vladismarkin@gmail.com>
2021-02-15 22:32:22 +0200

    Creating an interface. now TS telling us that we have some issues in our code

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,8 +2,16 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+// Interfaces in TS is used to discribe the stracture of the object.
+// We can ignore properties if we want to...
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((res) => {
-  const todo = res.data;
+  const todo = res.data as Todo;
 
   const ID = todo.ID;
   const title = todo.Title;
